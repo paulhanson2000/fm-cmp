c93aa1ed124541464e8b38d8d1706cc366e7ceca
Paul Hanson <phanso1@beluga1.int.ets1.calculquebec.ca>
2023-05-24 11:03:25 -0400

    LD matrix calculation works, but haven't yet verified if ref alleles match

--- scratch/run_paintor.sh
+++ scratch/run_paintor.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 ./PAINTOR -input input.files -in paintor_in/ -out paintor_out/ \
   -Zhead z_eas \
-  -LDname ld_eas \
+  -LDname ld_corr_eas \
   -annotations dummy_annot \
   -enumerate 1
