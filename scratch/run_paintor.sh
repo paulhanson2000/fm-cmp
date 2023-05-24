8360af7bf2f4d9f794d4aac6d8c5e089c7a81dfa
Paul Hanson <phanso1@beluga1.int.ets1.calculquebec.ca>
2023-05-24 15:18:01 -0400

    Ensure order of variants matches reference (i.e. same ordering in locus file and LD matrix). Ensure reference alleles match (I think?). PAINTOR works (or seems to) w/o warnings!

--- scratch/run_paintor.sh
+++ scratch/run_paintor.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 ./PAINTOR -input input.files -in paintor_in/ -out paintor_out/ \
-  -Zhead z_eas \
-  -LDname ld_corr_eas \
+  -Zhead z_eas,z_eur,z_sas \
+  -LDname ld_corr_eas,ld_corr_eur,ld_corr_sas \
   -annotations dummy_annot \
   -enumerate 1
