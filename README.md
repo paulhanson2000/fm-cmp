00a9891d787564cf5b3c8107cec0025d49f78a5a
Paul Hanson <phanso1@beluga4.int.ets1.calculquebec.ca>
2023-05-25 16:18:35 -0400

    organize scratch folder. SuSiEx working.

--- README.md
+++ README.md
@@ -5,10 +5,12 @@
     - [SuSiE-inf and FINEMAP-inf](https://github.com/FinucaneLab/fine-mapping-inf) ([paper](https://doi.org/10.1101/2022.10.21.513123))
   - [PAINTOR](https://github.com/gkichaev/PAINTOR_V3.0) ([paper1](https://doi.org/10.1371/journal.pgen.1004722), [paper2](https://www.cell.com/ajhg/fulltext/S0002-9297(15)00243-8), [paper3](https://doi.org/10.1093%2Fbioinformatics%2Fbtw615))
   - [MsCAVIAR](https://github.com/nlapier2/MsCAVIAR) ([paper](https://doi.org/10.1371/journal.pgen.1009733))
-  - [MA_FOCUS](https://www.mancusolab.com/ma-focus) ([paper](https://www.cell.com/ajhg/fulltext/S0002-9297(22)00306-8))
+  - [MA-FOCUS](https://www.mancusolab.com/ma-focus) ([paper](https://www.cell.com/ajhg/fulltext/S0002-9297(22)00306-8))
   - [GUESSFM](https://github.com/chr1swallace/GUESSFM) ([paper](https://doi.org/10.1371/journal.pgen.1005272))
   - [FINEMAP](http://www.christianbenner.com/) ([paper1](https://doi.org/10.1093/bioinformatics/btw018), [paper2](https://doi.org/10.1016/j.ajhg.2017.08.012), [paper3](https://doi.org/10.1101/318618))
   - [CAVIARBF](https://bitbucket.org/Wenan/caviarbf/src/master/) ([paper](https://doi.org/10.1534/genetics.116.188953))
+  - MANTRA ([paper](https://doi.org/10.1002/gepi.20630))
+  - [CAVIAR](https://github.com/fhormoz/caviar) ([paper1](https://doi.org/10.1534/genetics.114.167908), [paper2](https://doi.org/10.1016/j.ajhg.2016.10.003))
 + Try incorporating functional annotations if possible, if not already built-in to the method. (e.g. [fgwas](https://github.com/joepickrell/fgwas))
 + Try using the most suitable multi-ancestry methods on traits other than T2D (e.g. HbA1C, fasting insulin, MAGIC fasting glucose...)
 
@@ -18,4 +20,4 @@
 ...
 ## To document
 <s/t s/t symbolic link (only works for our specific setup on CCDB, explain how to set things up otherwise)>
-
+<run init_submodules script>
---
+++ scratch/susiex/susiex.sh
@@ -0,0 +1,48 @@
+#!/bin/bash
+
+# module purge # Good practice to do this, but comment out for a speedup
+module load python/3.11.2
+module load scipy-stack
+module load plink/1.9b_6.21-x86_64 # `module load plink/2.00a3.6` (plink 2.0) doesn't seem to work
+
+sumstats_eas=${SCRATCH}/DIAMANTE-EAS.sumstat-for_susiex.txt
+sumstats_eur=${SCRATCH}/DIAMANTE-EUR.sumstat-for_susiex.txt
+sumstats_sas=${SCRATCH}/DIAMANTE-SAS.sumstat-for_susiex.txt
+cat "../../data/DIAMANTE2022/sumstat/DIAMANTE-EAS.sumstat.txt" | sed -e '1 s/Fixed-effects_beta/BETA/' | grep -Eiv "NA|NaN" | awk '$5=toupper($5)' | awk '$6=toupper($6)' > ${sumstats_eas}
+cat "../../data/DIAMANTE2022/sumstat/DIAMANTE-EUR.sumstat.txt" | sed -e '1 s/Fixed-effects_beta/BETA/' | grep -Eiv "NA|NaN" | awk '$5=toupper($5)' | awk '$6=toupper($6)' > ${sumstats_eur}
+cat "../../data/DIAMANTE2022/sumstat/DIAMANTE-SAS.sumstat.txt" | sed -e '1 s/Fixed-effects_beta/BETA/' | grep -Eiv "NA|NaN" | awk '$5=toupper($5)' | awk '$6=toupper($6)' > ${sumstats_sas}
+
+ld_eas=${SCRATCH}/susiex_ld_eas
+ld_eur=${SCRATCH}/susiex_ld_eur
+ld_sas=${SCRATCH}/susiex_ld_sas
+
+ref_eas=../../data/ref/1kg/plink_format/eas/g1000_eas
+ref_eur=../../data/ref/1kg/plink_format/eur/g1000_eur
+ref_sas=../../data/ref/1kg/plink_format/sas/g1000_sas
+
+python3 ../../third_party/SuSiEx/SuSiEx.py \
+  --sst_file=${sumstats_eas},${sumstats_eur},${sumstats_sas} \
+  --n_gwas=56268,80154,16540 `#39,32,15` \
+  --ld_file=${ld_eas},${ld_eur},${ld_sas} `# If pre-existing file(s), computation will be skipped ` \
+  --ref_file=${ref_eas},${ref_eur},${ref_sas} \
+  --plink=plink \
+  --out_dir=out \
+  --out_name=test \
+  --chr=2 `#TODO` \
+  --bp=27230940,28230940 `#TODO` \
+  --chr_col=1,1,1 `# chromosome(b37) col` \
+  --snp_col=4,4,4 `# rsID col` \
+  --bp_col=2,2,2  `# position(b37) col` \
+  --a1_col=5,5,5  `# effect_allele` \
+  --a2_col=6,6,6  `# other_allele` \
+  --eff_col=8,8,8 `# Fixed-effects_beta` \
+  --se_col=9,9,9  `# Fixed-effects_SE` \
+  --pval_col=10,10,10 `# Fixed-effects_p-value` \
+  --mult-step=True \
+  --maf=0.01 \
+  --keep-ambig=True
+  # defaults: --max_iter=100, --pval_thresh=1e-5, --tol=1e-4, --n_sig=5, --level=95%, --min_purity=0.5, --full_out=NULL
+
+
+#rm ${sumstats_eas} ${sumstats_eur} ${sumstats_sas}
+#rm ${SCRATCH}/susiex_ld*
