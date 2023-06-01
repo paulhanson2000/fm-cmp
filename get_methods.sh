131a9653f842dd5e84288034050129ba4c82e299
Paul Hanson <phanso1@beluga1.int.ets1.calculquebec.ca>
2023-06-01 17:30:38 -0400

    Work on run.qmd. Input now via config files

--- get_methods.sh
+++ get_methods.sh
@@ -21,7 +21,7 @@ cd ./third_party/MsCAVIAR/
 if ! [ -f MsCAVIAR ]; then
   make
   if [ $? -ne 0 ]; then # Didn't work, try Flexiblas.
-    sed -i 's/-llapack -lblas/-lflexiblas/' Makefile # Change makefile to use flexiblas (lol, cursed)
+    sed -i 's/-llapack -lblas/-lflexiblas/' Makefile # Edit makefile to use flexiblas (lol, cursed)
     make
     sed -i 's/-lflexiblas/-llapack -lblas/' Makefile # Undo, in case this script needs to be run again.
   fi
