0dc7a0261295cc8b493afd799159f00d04b11eff
paulhanson2000 <paulhanson2000@gmail.com>
2024-01-26 12:40:03 -0500

    PLINK LD format conversion tests

--- old/plink_ld_format/read.R
+++ old/plink_ld_format/read.R
@@ -2,4 +2,19 @@ library(data.table)
 
 frq <- fread("ld_output/EUR_LD_frq.frq")
 bim <- fread("ld_output/EUR_LD_ref.bim")
-ld <- readBin("ld_output/EUR_LD.ld.bin", what="numeric", n=nrow(frq), size=4)
+ld_og <- readBin("ld_output/EUR_LD.ld.bin", what="numeric", n=nrow(frq)^2, size=4)
+ld    <- matrix(ld_og, nrow=nrow(frq), ncol=nrow(frq))
+
+isSymmetric(ld)
+# Matrix is symmetric, good
+
+ld2 <- as.data.table(ld)
+all(sapply(seq(1,nrow(frq)), function(i) ld2[[i,i]]==1))
+rm(ld2)
+# All diagonal entries are 1, good
+
+          # v equiv to ld_og
+writeBin(as.vector(ld), "ld_rewritten.ld.bin", size=4)
+ld_rewritten <- readBin("ld_rewritten.ld.bin", what="numeric", n=nrow(frq)^2, size=4)
+identical(ld_og,ld_rewritten)
+# TRUE, good
--- old/plink_ld_format/script.sh
+++ old/plink_ld_format/script.sh
@@ -1,7 +1,8 @@
+mkdir ld_output
 python2 ./SuSiEx_LD.py \
   --ref_file=1kg_eur_plink_format/g1000_eur \
   --ld_file=ld_output/EUR_LD \
   --chr=1 \
   --bp=7314654,8314677 \
-  --plink=plink \
+  --plink=./plink \
   --maf=0.005
